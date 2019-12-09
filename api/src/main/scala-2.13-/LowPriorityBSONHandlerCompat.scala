6faaf4e28355e7f5736c2d678d6ce4af1592d4ac
Thibault Duplessis <thib@fea.st>
2019-12-09 07:40:45 -0600

    Make the collection reader thread safe (#71)
    If a collection reader is reused, then the builder endlessly accumulates past values, leading to undesirable behaviour.
    
    A solution would be to clear the buffer after use, but the reader would still not be thread safe.
    
    Thread safety can be achieved by instantiating the buffer on demand.

--- api/src/main/scala-2.13-/LowPriorityBSONHandlerCompat.scala
+++ api/src/main/scala-2.13-/LowPriorityBSONHandlerCompat.scala
@@ -7,6 +7,14 @@ import scala.collection.generic.CanBuildFrom
 private[bson] trait LowPriorityBSONHandlersCompat {
   self: LowPriority1BSONHandlers =>
 
-  implicit final def collectionReader[M[_], T](implicit cbf: CanBuildFrom[M[_], T, M[T]], reader: BSONReader[T]): BSONReader[M[T]] = new BSONArrayCollectionReader(cbf())
-
+  implicit final def collectionReader[M[_], T](
+    implicit
+    cbf: CanBuildFrom[M[_], T, M[T]],
+    reader: BSONReader[T]): BSONReader[M[T]] = {
+    @inline def r = reader
+    new BSONArrayCollectionReader[M, T] {
+      val reader = r
+      def builder() = cbf()
+    }
+  }
 }
