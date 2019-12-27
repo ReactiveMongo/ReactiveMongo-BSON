b6e4849538a1b01f7ff13c15d1d2282ffa01c549
Cédric Chantepie <cchantep@users.noreply.github.com>
2019-12-27 17:04:55 +0100

    Bump version

--- project/build.sh
+++ project/build.sh
@@ -12,7 +12,7 @@ sbt $SBT_OPTS $SBT_CMDS
 TMP=`mktemp`
 tar -cvf "$TMP" `find . -path '*/target/*' -type f \( -name '*.jar' -or -name '*.pom' \) -print`
 
-REACTIVEMONGO_SHADED=false
+export REACTIVEMONGO_SHADED=false
 sbt $SBT_OPTS $SBT_CMDS
 
 tar -xvf "$TMP"
