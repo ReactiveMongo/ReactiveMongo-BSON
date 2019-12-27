c3417d1febb44acfda184404511a954de08a793a
Cédric Chantepie <cchantep@users.noreply.github.com>
2019-12-27 16:53:48 +0100

    Minor release 0.19.6

--- project/build.sh
+++ project/build.sh
@@ -2,9 +2,18 @@
   
 set -e
 
-SBT_CMDS="+makePom +packageBin +packageSrc +packageDoc"
+DIR=`dirname $0 | sed -e "s|^./|$PWD/|"`
 
-sbt $SBT_OPTS +clean $SBT_CMDS
+cd "$DIR/.."
+
+SBT_CMDS="+clean +makePom +packageBin +packageSrc +packageDoc"
+
+sbt $SBT_OPTS $SBT_CMDS
+TMP=`mktemp`
+tar -cvf "$TMP" `find . -path '*/target/*' -type f \( -name '*.jar' -or -name '*.pom' \) -print`
 
 REACTIVEMONGO_SHADED=false
 sbt $SBT_OPTS $SBT_CMDS
+
+tar -xvf "$TMP"
+rm -f "$TMP"
