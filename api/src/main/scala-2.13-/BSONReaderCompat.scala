df6210398ae05c549711f2e9df805ab16f979121
Cédric Chantepie <cchantep@users.noreply.github.com>
2020-07-15 13:00:00 +0200

    Add example to .iterable Scaladoc

--- api/src/main/scala-2.13-/BSONReaderCompat.scala
+++ api/src/main/scala-2.13-/BSONReaderCompat.scala
@@ -12,6 +12,17 @@ private[bson] trait BSONReaderCompat { self: BSONReader.type =>
    *
    * Creates a [[BSONReader]] accepting only [[BSONArray]],
    * and applying the given safe `read` function to each element value.
+   *
+   * {{{
+   * import reactivemongo.api.bson.{ BSONReader, Macros }
+   *
+   * case class Element(str: String, v: Int)
+   *
+   * val elementHandler = Macros.handler[Element]
+   *
+   * val setReader: BSONReader[Set[Element]] =
+   *   BSONReader.iterable[Element, Set](elementHandler readTry _)
+   * }}}
    */
   def iterable[T, M[_]](read: BSONValue => Try[T])(
     implicit
