19e7b015b5194fa699a40a87e14225e3f3cc0f9a
Cédric Chantepie <cchantep@users.noreply.github.com>
2020-07-15 23:09:37 +0200

    Fix build script

--- project/build.sh
+++ project/build.sh
@@ -10,8 +10,4 @@ SBT_CMDS="+clean +makePom +packageBin +packageSrc +packageDoc"
 
 sbt $SBT_OPTS $SBT_CMDS
 
-export REACTIVEMONGO_SHADED=false
-sbt $SBT_OPTS $SBT_CMDS
-
-tar -xvf "$TMP"
-rm -f "$TMP"
+REACTIVEMONGO_SHADED=false sbt $SBT_OPTS $SBT_CMDS
