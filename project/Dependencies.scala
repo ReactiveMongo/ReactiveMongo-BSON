aab7ddaa259dd3ce892e38648c1a1acbe0d06759
Scala Steward <me@scala-steward.org>
2021-07-20 15:50:56 +0200

    Update slf4j-simple to 1.7.32

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,5 +7,5 @@ object Dependencies {
     "org.specs2" %% "specs2-core" % specsVer,
     "org.specs2" %% "specs2-junit" % specsVer)
 
-  val slf4jApi = "org.slf4j" % "slf4j-simple" % "1.7.31"
+  val slf4jApi = "org.slf4j" % "slf4j-simple" % "1.7.32"
 }
