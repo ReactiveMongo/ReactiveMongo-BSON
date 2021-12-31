63d8f7ace206b523fe02cfd291c91807397899cd
Cédric Chantepie <cchantep@users.noreply.github.com>
2021-12-31 23:27:33 +0100

    Failure on default value

--- api/src/test/scala-3/MacroCompilation.scala
+++ api/src/test/scala-3/MacroCompilation.scala
@@ -11,4 +11,21 @@ object MacroCompilation:
       ${ MacroImpl.implicitOptionsConfig }
 
   end WithConfig
+
 end MacroCompilation
+
+final class OtherCompilation {
+
+  case class Product(
+      _id: Int,
+      sku: Option[String] = None,
+      description: Option[String] = None,
+      instock: Option[Int] = None)
+
+  def foo(): Unit = {
+    // See https://github.com/ReactiveMongo/ReactiveMongo-BSON/commit/d1a776b19e6ee07e868d3baa16c0c1ade8799fad#r62747279
+    implicit val productHandler: BSONDocumentHandler[Product] =
+      Macros.handler[Product]
+    ()
+  }
+}
