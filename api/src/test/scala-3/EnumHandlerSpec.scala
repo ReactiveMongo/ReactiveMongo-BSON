8a6c8c68030dbe99ae40a1ad5e87057f52401380
Cédric Chantepie <cchantep@users.noreply.github.com>
2022-02-08 22:38:09 +0100

    Fix tests for Scala 3 enum handlers

--- api/src/test/scala-3/EnumHandlerSpec.scala
+++ api/src/test/scala-3/EnumHandlerSpec.scala
@@ -23,73 +23,71 @@ final class EnumHandlerSpec extends org.specs2.mutable.Specification {
     handler = EnumHandler.handler[Dummy]
   )
 
-  /*
   testKeyScenario(
     descriptor = "normal operation (no transformations)",
-    reader = EnumHandler.keyReader(Dummy),
+    reader = EnumHandler.keyReader[Dummy],
     expectedReadSuccesses = Map("A" -> Dummy.A, "c" -> Dummy.c),
     expectedReadFails = Seq("C"),
-    writer = EnumHandler.keyWriter(Dummy),
+    writer = EnumHandler.keyWriter[Dummy],
     expectedWrites = Map(Dummy.A -> "A", Dummy.c -> "c")
   )
 
   testScenario(
     descriptor = "case insensitive",
-    reader = EnumHandler.reader(Dummy, insensitive = true),
+    reader = EnumHandler.reader[Dummy](insensitive = true),
     expectedReadSuccesses = Map("A" -> Dummy.A, "a" -> Dummy.A, "C" -> Dummy.c),
     expectedReadFails = Nil,
-    writer = EnumHandler.writer(Dummy),
+    writer = EnumHandler.writer[Dummy],
     expectedWrites = Map(Dummy.A -> "A", Dummy.c -> "c"),
-    handler = EnumHandler.handler(Dummy, insensitive = true)
+    handler = EnumHandler.handler[Dummy](insensitive = true)
   )
 
   testKeyScenario(
     descriptor = "case insensitive",
-    reader = EnumHandler.keyReader(Dummy, insensitive = true),
+    reader = EnumHandler.keyReader[Dummy](insensitive = true),
     expectedReadSuccesses = Map("A" -> Dummy.A, "a" -> Dummy.A, "C" -> Dummy.c),
     expectedReadFails = Nil,
-    writer = EnumHandler.keyWriter(Dummy),
+    writer = EnumHandler.keyWriter[Dummy],
     expectedWrites = Map(Dummy.A -> "A", Dummy.c -> "c")
   )
 
   testScenario(
     descriptor = "lower case transformed",
-    reader = EnumHandler.readerLowercaseOnly(Dummy),
+    reader = EnumHandler.readerLowercaseOnly[Dummy],
     expectedReadSuccesses = Map("a" -> Dummy.A, "b" -> Dummy.B, "c" -> Dummy.c),
     expectedReadFails = Seq("A", "B", "C"),
-    writer = EnumHandler.writerLowercase(Dummy),
+    writer = EnumHandler.writerLowercase[Dummy],
     expectedWrites = Map(Dummy.A -> "a", Dummy.c -> "c"),
-    handler = EnumHandler.handlerLowercaseOnly(Dummy)
+    handler = EnumHandler.handlerLowercaseOnly[Dummy]
   )
 
   testKeyScenario(
     descriptor = "lower case transformed",
-    reader = EnumHandler.keyReaderLowercaseOnly(Dummy),
+    reader = EnumHandler.keyReaderLowercaseOnly[Dummy],
     expectedReadSuccesses = Map("a" -> Dummy.A, "b" -> Dummy.B, "c" -> Dummy.c),
     expectedReadFails = Seq("A", "B", "C"),
-    writer = EnumHandler.keyWriterLowercase(Dummy),
+    writer = EnumHandler.keyWriterLowercase[Dummy],
     expectedWrites = Map(Dummy.A -> "a", Dummy.c -> "c")
   )
 
   testScenario(
     descriptor = "upper case transformed",
-    reader = EnumHandler.readerUppercaseOnly(Dummy),
+    reader = EnumHandler.readerUppercaseOnly[Dummy],
     expectedReadSuccesses = Map("A" -> Dummy.A, "B" -> Dummy.B, "C" -> Dummy.c),
     expectedReadFails = Seq("c"),
-    writer = EnumHandler.writerUppercase(Dummy),
+    writer = EnumHandler.writerUppercase[Dummy],
     expectedWrites = Map(Dummy.A -> "A", Dummy.c -> "C"),
-    handler = EnumHandler.handlerUppercaseOnly(Dummy)
+    handler = EnumHandler.handlerUppercaseOnly[Dummy]
   )
 
   testKeyScenario(
     descriptor = "upper case transformed",
-    reader = EnumHandler.keyReaderUppercaseOnly(Dummy),
+    reader = EnumHandler.keyReaderUppercaseOnly[Dummy],
     expectedReadSuccesses = Map("A" -> Dummy.A, "B" -> Dummy.B, "C" -> Dummy.c),
     expectedReadFails = Seq("c"),
-    writer = EnumHandler.keyWriterUppercase(Dummy),
+    writer = EnumHandler.keyWriterUppercase[Dummy],
     expectedWrites = Map(Dummy.A -> "A", Dummy.c -> "C")
   )
-   */
 
   private def testScenario(
       descriptor: String,
