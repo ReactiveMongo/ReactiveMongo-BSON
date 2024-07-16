01f7d8d6f33b5abb6d193435fd68b296e7c03655
Cédric Chantepie <cchantep@users.noreply.github.com>
2024-07-16 22:52:40 +0200

    Update snapshot script

--- project/snapshot.sh
+++ project/snapshot.sh
@@ -2,8 +2,11 @@
 
 export PUBLISH_REPO_NAME="Sonatype Nexus Repository Manager"
 export PUBLISH_REPO_ID="oss.sonatype.org"
-#export PUBLISH_USER=""
 export PUBLISH_REPO_URL="https://oss.sonatype.org/content/repositories/snapshots"
+
+if [ -z "$PUBLISH_USER" ]; then
+  export PUBLISH_USER="$USER"
+fi
 
 echo "Password: "
 read PASS
