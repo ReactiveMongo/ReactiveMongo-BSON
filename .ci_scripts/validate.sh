0cede3786e41d67ea1fc193837a4b802a95eb6cd
Cédric Chantepie <cchantep@users.noreply.github.com>
2026-03-19 13:58:28 +0100

    Update build plugin

--- .ci_scripts/validate.sh
+++ .ci_scripts/validate.sh
@@ -12,17 +12,17 @@ if [ "v${SCALA_VERSION}" != "v2.11.12" ]; then
     SBT_TASK="$SBT_TASK ;scalafixAll -check"
 fi
 
+source "$SCRIPT_DIR/jvmopts.sh"
+
+export JVM_OPTS
+export SBT_OPTS
+
 sbt ++$SCALA_VERSION "$SBT_TASK" || (
   echo "ERROR: Scalafmt check failed, see differences above."
   echo "To fix, format your sources using ./build scalafmtAll before submitting a pull request."
   echo "Additionally, please squash your commits (eg, use git commit --amend) if you're going to update this pull request."
   false
 )
-
-source "$SCRIPT_DIR/jvmopts.sh"
-
-export JVM_OPTS
-export SBT_OPTS
 
 TEST_ARGS=";error ;test:compile ;mimaReportBinaryIssues ;warn ;testOnly ;doc"
 
